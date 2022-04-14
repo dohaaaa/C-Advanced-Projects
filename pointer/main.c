0a75537b1a1de0fc82f3e34a041db11bde84d083
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-15 00:07:57 +0200

    Add files via upload

--- pointer/main.c
+++ pointer/main.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+   /* int x=5;
+    int *p=&x;
+    printf("%d",*p);*/
+
+
+
+ void swap(int *x,int *y);
+
+
+int main()
+{
+
+    int x=1;
+    int y=2;
+
+   printf("the value before swapping is :%d %d\n",x,y);
+
+   swap(&x,&y);
+   printf("the value after swapping is :%d %d\n",x,y);
+
+
+}
+
+  void swap(int *x,int *y){
+
+  int temp;
+  temp=*x;
+  *x=*y;
+  *y=temp;
+
+
+
+
+  }
