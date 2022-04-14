ddb9530d96c7d90ab0cb78f9850d9fe6a2e8f1aa
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-14 23:53:55 +0200

    Add files via upload

--- variables/main.c
+++ variables/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+   int doha_age=2022-2005 ;
+   float temp=31.5;
+   char letter='A';
+   int bag=200;
+
+   printf("my age is:%d\n",doha_age);
+   printf("the temp is :%f\n",temp);
+   printf("the letter is %c\n",letter);
+   printf("the bag price :%d$\n",bag);
+    return 0;
+}
