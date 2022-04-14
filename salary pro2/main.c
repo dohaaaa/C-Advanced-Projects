af84a7b30b5420c25be38f8d8440f6772b9f75d8
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-15 00:01:35 +0200

    Add files via upload

--- salary pro2/main.c
+++ salary pro2/main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+
+    float salary;
+    float sales;
+    float hrsworked;
+    printf("Enter your salary:\n");
+    scanf("%f",& salary);
+    printf("Enter your sales:\n");
+    scanf("%f",& sales);
+    printf("Enter your hrsworked:\n");
+    scanf("%f",& hrsworked);
+
+    (sales>25000 || hrsworked> 40)? printf(" bonus ,your salary will to be :%f", salary+(salary*0.10)):printf("no bonus.");
+    !(sales>25000 || hrsworked> 40)?printf(" discond ,your salary will to be :%f", salary-(salary*0.10)):printf("no discond.");
+
+
+
+
+
+
+    return 0;
+}
