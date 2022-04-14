d007a609eb7175a8286d94dcdc3ba1f2ad11442f
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-15 00:12:05 +0200

    Add files via upload

--- loop/main.c
+++ loop/main.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+
+     /* 1.count
+       2.check
+       3.++ --*/
+
+    int count=1;
+    while(count<=10){
+
+    printf("Hello world!%d\n",count);
+
+    count=count+1;
+    count++;
+    count=count+2;
+    count+=2;
+
+    }
+    int count=10;
+    while(count>=1){
+
+        printf("Hello world!%d\n",count);
+
+        count--;
+    }
+
+   for(int i=0;i<10;i++){
+
+    printf("Hello world!%d\n",i);
+
+   }
+
+}
