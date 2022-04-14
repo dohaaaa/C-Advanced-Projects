9c8fac17d0c063260819273327aa30c849f422ab
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-15 00:04:35 +0200

    Add files via upload

--- reacting/main.c
+++ reacting/main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int sad;
+    int love;
+
+    printf(" the number of sad\n");
+    scanf("%d",sad);
+
+    printf(" the number of love\n");
+    scanf("%d",love);
+
+    if(sad>love){
+        printf("sad post\n");
+    }
+    else if(sad==love){
+        printf("feelings post");
+    }
+    else{
+        printf("loving post");
+
+    }
+
+    return 0;
+}
