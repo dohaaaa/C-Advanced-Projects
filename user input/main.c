f94786edb6414022bea4a0f1d446369210af432e
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-14 23:56:49 +0200

    Add files via upload

--- user input/main.c
+++ user input/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+
+    float radius;
+     const float PI =3.14;
+    printf("enter the radius of the circle\n");
+    scanf("%f",&radius);
+    printf("the aria of circle is %f\n",(radius*radius)*3.14 );
+
+
+    int birthdate;
+    printf("Enter your birthdate");
+    scanf("%d",&birthdate);
+    printf("my age is :%d",2022-birthdate);
+
+
+
+
+
+
+    return 0;
+}
