d8612ecbbfa6cec8c7093a5cb2e657cb3508e0b1
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-14 23:58:34 +0200

    Add files via upload

--- sum project/main.c
+++ sum project/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    float sum=0;
+    int number;
+    float item;
+    printf("how many items you want?\n");
+    scanf("%f",&item);
+    for (int i=0;i<item;i++){
+      printf("Enter the item %d price:\n",i+1);
+      scanf("%d",&number);
+      sum=sum+number;
+
+    }
+    printf("the total price is :%f\n",sum);
+    printf("avg price is :%f \n",sum/item);
+}
