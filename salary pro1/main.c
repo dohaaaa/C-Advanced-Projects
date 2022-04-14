d8a3755b066f6de65d8561f82bb456de0c7bc202
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-14 23:51:54 +0200

    Add files via upload

--- salary pro1/main.c
+++ salary pro1/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int ahmadsalary=3000;
+    int dohasalary=6000;
+    float alisalary=2000;
+    float total=ahmadsalary+dohasalary+alisalary;
+    printf("BMW Cars Company\n");
+    printf("My Employee Salaries\n");
+    printf("ahmadsalary is %d\n",ahmadsalary);
+    printf("dohasalary is %d\n",dohasalary);
+    printf("alisalary is %d\n",alisalary);
+    printf("the total salary I have to effort are:%d \n",total);
+    printf("the avg salary are :%f\n",total /3);
+    printf("alisalary after the discound is %f\n",alisalary-alisalary*0.10);
+
+
+
+
+    return 0;
+}
