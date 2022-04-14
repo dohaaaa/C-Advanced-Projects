d259d1899999549c95760e5388f1e962798e03b3
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-15 00:13:57 +0200

    Add files via upload

--- laptop store/main.c
+++ laptop store/main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+
+   float laptop_price;
+
+   printf("Enter the laptop_price please!\n");
+
+   scanf("%f",&laptop_price);
+
+
+   printf("the added tax is :%f\n",laptop_price*0.05);
+
+   printf("the laptop_price after the added tax is:%f\n",laptop_price+laptop_price*0.05);
+
+
+
+    return 0;
+}
