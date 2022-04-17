c9622c7696edc3ddfddcaac399138c054ef86343
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-18 00:48:57 +0200

    Add files via upload

--- greedy algorithms/main.c
+++ greedy algorithms/main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int item=27;
+    int pay=100;
+    int remain=pay-item;
+    int hundred,half,ten,pound;
+
+    printf("the remain is :%d\n ",remain);
+
+    hundred=remain/100;
+    remain=remain%100;
+    printf("your hundreds are :%d\n",hundred);
+
+    half=remain/50;
+    remain=remain%50;
+    printf(" your half is :%d\n",half);
+
+    ten=remain/10;
+    remain=remain%10;
+    printf(" your ten is :%d\n",ten);
+
+    printf(" your pounds are :%d\n",remain);
+
+    return 0;
+
+
+
+
+
+}
