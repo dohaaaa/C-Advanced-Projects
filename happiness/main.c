5f6fa6889c44fdf85217b4301c268e5e12a85f79
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-04-18 00:47:46 +0200

    Add files via upload

--- happiness/main.c
+++ happiness/main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+
+  int prefer;
+  printf("on a scale of 1 to 10,how happy are you?\n");
+  scanf("%d",&prefer);
+
+  if(prefer>=8){
+    printf("PRIMA");
+  }
+  else if(prefer>=5){
+    printf("SO GUT ");
+  }
+  else if(prefer>=3){
+    printf("GUT");
+  }
+  else{
+    printf("SCHLECHT");
+  }
+
+
+
+    return 0;
+}
