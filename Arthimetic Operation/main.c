a38333556dee8062eaa132645c50af29eaeaba6f
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-05-28 13:41:19 +0200

    Add files via upload

--- Arthimetic Operation/main.c
+++ Arthimetic Operation/main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    printf("After 25 years from now it will be :%d\n",2022+17);
+    printf("My age is :%d\n",2022-2005);
+    printf("The order after the service :%f \n",25+25*0.05);
+    printf("The number of rooms they need: %d rooms \n",50/5);
+    return 0;
+}
