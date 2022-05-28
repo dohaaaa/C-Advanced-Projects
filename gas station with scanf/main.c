f19e6ce4683a19e85e766f76927513bf4a06a060
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-05-28 13:23:47 +0200

    Add files via upload

--- gas station with scanf/main.c
+++ gas station with scanf/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+   float liter;
+   printf("Enter how many liters you want\a?\n");
+   scanf("%f",&liter);
+   printf("For gasoline 80 you will pay:%f\n",liter*7.25);
+   printf("For gasoline 92 you will pay:%f\n",liter*8.50);
+   printf("For gasoline 95 you will pay:%f\n",liter*9.50);
+
+
+    return 0;
+}
