a39682b0f8f1e0169475751bfbe7d3fa6eee3913
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-05-28 13:38:05 +0200

    Add files via upload

--- birthdate project/main.c
+++ birthdate project/main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+   int currentyear=2022;
+   int birthdate=2005;
+   int year=currentyear- birthdate;
+   int month,week,day;
+
+   printf(" your year is=%d\n",year);
+
+   day=year*365;
+   year=year%365;
+    printf(" your day is=%d\n",day);
+
+    week=year*48;
+   year=year%48;
+    printf(" your week is=%d\n",week);
+
+     month=year*12;
+   year=year%12;
+    printf(" your month is=%d\n",month);
+
+
+
+}
