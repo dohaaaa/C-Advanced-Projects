789ff92406b9dab1b899493daa82a5eddf63d54e
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-05-28 13:43:52 +0200

    Add files via upload

--- And , Or/main.c
+++ And , Or/main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int age;
+    printf("Enter your age: \n");
+    scanf("%d",age);
+    (age>=18)?printf("legal"):printf("not legal");
+
+
+
+    int age;
+    printf("Enter your age :\n");
+    scanf("%d",age);
+    if(age>=18 && age<30){
+        printf("Legal");
+    }
+    else{
+       printf(" Not Legal");
+    }
+
+
+
+
+
+    return 0;
+}
