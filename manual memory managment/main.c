dbfc0d9f7d4cc5f4a377975cd5e7af7fc762e314
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-05-28 13:49:48 +0200

    Add files via upload

--- manual memory managment/main.c
+++ manual memory managment/main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int x=5;
+
+    int *p=malloc(10*sizeof(int));
+
+    if(p==NULL){
+
+        printf("there is no memory allocated");
+    }
+    else{
+        printf("there is  memory and we allocated for you");
+
+       free(p);
+    }
+    char c=12000;
+    printf("%d\n",c);
+
+    char *z=malloc(sizeof(char)*5);
+
+    z=12000;
+    printf("%d\n",z);
+
+
+
+}
