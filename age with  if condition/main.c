75b1ef3d05fdd60166def459e8abdb5ce23bc1da
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-05-28 13:45:01 +0200

    Add files via upload

--- age with  if condition/main.c
+++ age with  if condition/main.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int age;
+    printf("Enter your age:\n");
+    scanf("%d",&age);
+
+    if(age>=18){
+        printf("you are legal\n ");
+    }
+    else{
+        printf("you are illegal\n");
+    }
+
+    int number;
+    printf("Enter a number\n");
+    scanf("%d",&number);
+    if(number!=7){
+        printf("okey your number will be %d",number);
+    }
+    else{
+        printf("reserved\n");
+    }
+
+
+
+    return 0;
+}
