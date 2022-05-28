75b0eef84a8f113d0e25de8c2f6385af1da84957
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-05-28 13:25:36 +0200

    Add files via upload

--- Gas Station project/main.c
+++ Gas Station project/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+   int liter=50;
+   float eighgas=7.25;
+   float tninegas=8.50;
+   float fninegas=9.50;
+
+   printf("welcome to gas station!\n");
+
+   printf("i want 50 liters for 80gas ,how much?\n");
+   printf("it is =%f$\n",liter*eighgas);
+
+    printf("i want 50 liters for 92gas ,how much?\n");
+    printf("it is =%f$\n",liter*tninegas);
+
+    printf("i want 50 liters for 95gas ,how much?\n");
+    printf("it is =%f$\n",liter*fninegas);
+
+    return 0;
+
+
+}
