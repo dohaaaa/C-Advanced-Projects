a36f4cff57640e3674428873770901fb8c768c56
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-05-28 13:46:05 +0200

    Add files via upload

--- for/main.c
+++ for/main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /*for(int i=1;i<=10;i++){
+
+
+     for(int j=1;j<=10;j++){
+    printf("Hello world!%d\n",j);
+
+    }*/
+
+   printf("how many level for your pyramid\n");
+ int level;
+
+ scanf("%d",&level);
+ level=level+2;
+ for (int i=2;i,level;i++){
+    for(int j=1;j<i;j++)
+        {
+        printf("#");
+    }
+    printf("\n");
+ }
+
+
+
+
+
+}
+
 
