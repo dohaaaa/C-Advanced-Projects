11a5787f12568b07486bd0c87f3e7cf40eaeae0d
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-06-02 01:34:04 +0200

    Add files via upload

--- search/main.c
+++ search/main.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+   /*worst case
+     average case
+     best case
+   */
+   //liner search
+   int arr[]={1,2,3,4,5,6,7,8,9,10};
+   int number=20;
+   for(int i=0;i<10;i++){
+
+        if(arr[i]==number){
+            printf("%d\n",i);
+            return i;
+        }
+   }
+   printf("Not found\n");
+   return 0;
+
+
+
+
+
+   char names[]={'mohamed','doha','ali'};
+   char name='ali';
+   for(char i=0;i<3;i++){
+
+        if(names[i]==name){
+            printf("%d\n",i);
+            return i;
+        }
+   }
+   printf("Not found\n");
+   return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
