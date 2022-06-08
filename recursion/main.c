4f689519b36c58f0fe1402d249b9d0a68ffd73ae
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-06-08 19:53:53 +0200

    Add files via upload

--- recursion/main.c
+++ recursion/main.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+   int factorial(int n){
+
+   if(n==1){
+    return 1;
+   }
+   else{
+    return n*factorial(n-1);
+   }
+
+   }
+
+
+   int countdown(int n){
+
+    printf("%d\n",n)
+    if(n<=0){
+        return 0;
+
+    }
+    else{
+        return countdown(n-1);
+    }
+    }
+
+    int main(){
+    countdown(10);
+    printf(factorial(10));
+
+    }
+
+
+
 
