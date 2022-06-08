bd09b28d08befa5cdc6600e3af3ebf99f03e2ee6
dohaaaa <103540236+dohaaaa@users.noreply.github.com>
2022-06-08 18:31:55 +0200

    Add files via upload

--- dynamic programming/main.c
+++ dynamic programming/main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+     char email[30];
+     printf("enter your email:\n");
+     scanf("%s",email);
+     printf("your email is :%s",email);
+
+   for(int i=0;i<10;i++){
+    if(email[i]=='@'){
+        printf("\nvailed email");
+        return 0;
+    }
+
+   }
+    printf("\nin vailed email");
+
+}
